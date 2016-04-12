274b9bd37b9f487089283c4e2c02ce28262c5268
Dan Lewis <dan.lewis@devacademy.co.nz>
2016-04-12 15:00:06 +1200

    update js with doc ready

--- assets/material-drawer-right.js
+++ assets/material-drawer-right.js
@@ -1,17 +1,19 @@
-$('#cart').click(function(){
- if($('.mdl-layout__drawer-right').hasClass('active')){       
-    $('.mdl-layout__drawer-right').removeClass('active'); 
- }
- else{
-    $('.mdl-layout__drawer-right').addClass('active'); 
- }
-});
+$(function() {
+  $('#cart').click(function(){
+   if($('.mdl-layout__drawer-right').hasClass('active')){       
+      $('.mdl-layout__drawer-right').removeClass('active'); 
+   }
+   else{
+      $('.mdl-layout__drawer-right').addClass('active'); 
+   }
+  });
 
-$('.mdl-layout__obfuscator-right').click(function(){
- if($('.mdl-layout__drawer-right').hasClass('active')){       
-    $('.mdl-layout__drawer-right').removeClass('active'); 
- }
- else{
-    $('.mdl-layout__drawer-right').addClass('active'); 
- }
-});
+  $('.mdl-layout__obfuscator-right').click(function(){
+   if($('.mdl-layout__drawer-right').hasClass('active')){       
+      $('.mdl-layout__drawer-right').removeClass('active'); 
+   }
+   else{
+      $('.mdl-layout__drawer-right').addClass('active'); 
+   }
+  });
+}
