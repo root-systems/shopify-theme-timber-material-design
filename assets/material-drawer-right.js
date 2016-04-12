b0afbc506abb7b9276c25533f168132a6296a25c
Dan Lewis <dan.lewis@devacademy.co.nz>
2016-04-12 14:47:25 +1200

    test with mdl drawer

---
+++ assets/material-drawer-right.js
@@ -0,0 +1,17 @@
+$('#cart').click(function(){
+ if($('.mdl-layout__drawer-right').hasClass('active')){       
+    $('.mdl-layout__drawer-right').removeClass('active'); 
+ }
+ else{
+    $('.mdl-layout__drawer-right').addClass('active'); 
+ }
+});
+
+$('.mdl-layout__obfuscator-right').click(function(){
+ if($('.mdl-layout__drawer-right').hasClass('active')){       
+    $('.mdl-layout__drawer-right').removeClass('active'); 
+ }
+ else{
+    $('.mdl-layout__drawer-right').addClass('active'); 
+ }
+});
