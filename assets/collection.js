f516f70d8edb4eb045f8c77fab1d576e890c0d83
Dan Lewis <dan.lewis@devacademy.co.nz>
2016-04-19 17:41:33 +1200

    back to js

--- assets/collection.js
+++ assets/collection.js
@@ -1,16 +1,16 @@
-// var cards = document.getElementsByClassName("collection-card");
+var cards = document.getElementsByClassName("collection-card");
 
-// for (var i = cards.length - 1; i >= 0; i--) {
-//   cards[i].addEventListener("mouseover", focus, false);
-//   cards[i].addEventListener("mouseout", unfocus, false);
-// };
+for (var i = cards.length - 1; i >= 0; i--) {
+  cards[i].addEventListener("mouseover", focus, false);
+  cards[i].addEventListener("mouseout", unfocus, false);
+};
 
-// function focus()
-// {
-//   this.lastElementChild.classList.add('collection-card__text-wrapper--focus')
-// }
+function focus()
+{
+  this.lastElementChild.classList.add('collection-card__text-wrapper--focus')
+}
 
-// function unfocus()
-// {  
-//   this.lastElementChild.classList.remove('collection-card__text-wrapper--focus')
-// }
+function unfocus()
+{  
+  this.lastElementChild.classList.remove('collection-card__text-wrapper--focus')
+}
